09869bfca2dd02038b158143a3078580c49de51d
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-07-30 23:19:53 +0700

    Add yargs for command-line note management; implement add, remove, read, and list commands in app.js. Update package.json and package-lock.json to include yargs dependency.

--- section-4/app.js
+++ section-4/app.js
@@ -1,12 +1,46 @@
 const chalk = require("chalk");
 const getNotes = require("./notes.js");
+const yargs = require("yargs");
 
-const command = process.argv[2];
+// Customize yargs version
+yargs.version("1.1.0");
 
-console.log(process.argv);
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: () => {
+    console.log("Adding a new note");
+  },
+});
 
-if (command === "add") {
-  console.log("Adding note!");
-} else if (command === "remove") {
-  console.log("Removing note!");
-}
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a new note",
+  handler: () => {
+    console.log("Removing a new note");
+  },
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a new note",
+  handler: () => {
+    console.log("Reading a new note");
+  },
+});
+
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler: () => {
+    console.log("Listing all notes");
+  },
+});
+
+// add, remove, read, list
+
+console.log("yargs", yargs.argv);
