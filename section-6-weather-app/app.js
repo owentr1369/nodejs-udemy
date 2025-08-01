90d3582bdc34dafc351f44fccf873927bedf607d
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-01 19:44:15 +0700

    handle errors

--- section-6-weather-app/app.js
+++ section-6-weather-app/app.js
@@ -3,8 +3,12 @@ const request = require("request");
 const url = "https://api.restful-api.dev/objects";
 
 request({ url, json: true }, (error, response) => {
-  const data = response.body;
-  const iPad = data.find((item) => item.id === "12");
-  console.log("iPad", iPad);
-  console.log(`The ${iPad.name} is currently $${iPad.data.Price}`);
+  if (error) {
+    console.log("Cannot connect to the service");
+  } else {
+    const data = response.body;
+    const iPad = data.find((item) => item.id === "12");
+    console.log("iPad", iPad);
+    console.log(`The ${iPad.name} is currently $${iPad.data.Price}`);
+  }
 });
