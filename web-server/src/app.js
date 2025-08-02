8efe692e76612a5f15ab0802b4a6fc3e726cfd3d
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-02 10:56:46 +0700

    serving up static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.send("Hello Express!");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Help page");
+  res.send({
+    name: "Owen",
+    age: 26,
+  });
 });
 
 app.get("/about", (req, res) => {
