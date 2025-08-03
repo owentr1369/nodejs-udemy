34ffc6313a629aac357c97f1f48acc30bccb0c22
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-03 12:03:33 +0700

    fetch users list

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -25,6 +25,16 @@ app.post("/users", (req, res) => {
     });
 });
 
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
   task
