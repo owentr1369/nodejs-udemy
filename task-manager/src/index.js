62053488741e9f84ae9833ff983d03baeffe6d2f
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-03 12:25:30 +0700

    deleting user

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -60,6 +60,18 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
   try {
