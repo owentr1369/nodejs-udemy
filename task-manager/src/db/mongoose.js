096c58a573e7ec1f5a9b75fa486b66155f989c90
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-03 12:00:57 +0700

    create task from postman

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -3,14 +3,3 @@ const mongoose = require("mongoose");
 const connectionURL = "mongodb://127.0.0.1:27017/task-manager-api";
 
 mongoose.connect(connectionURL);
-
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-  },
-});
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,6 +19,18 @@ app.post("/users", (req, res) => {
     .save()
     .then(() => {
       res.send(user);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(() => {
+      res.send(task);
     })
     .catch((err) => {
       res.send(err);
