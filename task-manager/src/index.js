0da85d8c725936abd1774361f46cc66efd846a1a
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-03 12:22:04 +0700

    user updating method

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -37,6 +37,29 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+app.patch("/users/:id", async (req, res) => {
+  const allowedUpdates = ["name", "email", "age"];
+  try {
+    const updates = Object.keys(req.body);
+    const isValidOperation = updates.every((update) =>
+      allowedUpdates.includes(update)
+    );
+    if (!isValidOperation) {
+      return res.status(400).send({ error: "Invalid updates!" });
+    }
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
   try {
