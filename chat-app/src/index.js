42d45b0a49db25e6484c7ba4f3ecd57026c7fc57
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-06 16:27:56 +0700

    Add socket.io for real-time communication and update server setup in chat app

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+const io = new Server(server);
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(3000, () => {
+io.on("connection", () => {
+  console.log("New web socket connection");
+});
+
+server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
