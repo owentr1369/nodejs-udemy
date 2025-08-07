23027d3c3053ce53a4a3e70ae7e44ecfd952055e
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-07 15:48:13 +0700

    storing users 2

--- chat-app/src/utils/users.js
+++ chat-app/src/utils/users.js
@@ -40,3 +40,19 @@ const removeUser = (id) => {
     return users.splice(index, 1)[0];
   }
 };
+
+const getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+
+const getUsersInRoom = (room) => {
+  room = room.trim().toLowerCase();
+  return users.filter((user) => user.room === room);
+};
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+};
